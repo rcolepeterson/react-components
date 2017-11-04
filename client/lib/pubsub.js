be221105cf2180e2db1828b85bbdb52a27790138
Cole Peterson <Cole.Peterson@possible.com>
2017-11-03 23:14:07 -0700

    added some tests, added some comments

--- client/lib/pubsub.js
+++ client/lib/pubsub.js
@@ -1,18 +1,33 @@
+/**
+ * Simple pub sub code.
+ */
 const observe = () => {
   let actions = {};
 
   return {
+    /**
+     * action {string} What u want to know about.
+     * l - {function} Callback.
+     */
     subscribe: (action, l) => {
+      if (!l){
+        return new Error('no callback');
+      }
+      // if no .. create
       if (actions.hasOwnProperty(action) === false) {
         actions[action] = [l];
       } else {
         actions[action].push(l);
       }
     },
-
-    publish: action => {
+    /**
+     * Dispatch msg.
+     * action {string} string to broadcast.
+     * value {*} Item to pass thru callback.
+     */
+    publish: (action, value) => {
       if (actions.hasOwnProperty(action)) {
-        actions[action].forEach(l => l());
+        actions[action].forEach(l => l(value));
       } else {
         console.warn(`no action matches ${action}`);
       }
--- tests/pubsub.test.js
+++ tests/pubsub.test.js
@@ -1,11 +1,18 @@
 import pubsub from "../client/lib/pubsub.js";
 import test from "tape";
-test("should exist", t => {
-  t.equals("object", typeof pubsub());
+const ps = pubsub();
+
+test("should return an object with a the correct API", t => {
+  t.equals("function", typeof ps.subscribe);
+  t.equals("function", typeof ps.publish);
   t.end();
 });
 
-test("should return an object with a subscibe method", t => {
-  t.equals("function", typeof pubsub().subscribe);
-  t.end();
+test("should fire a call back when a topic is published", (t) => {
+  let str = 'you am';
+  ps.subscribe('coolEvent', (value) => {
+    t.equal(value, str);
+    t.end();
+  });
+  ps.publish('coolEvent', str);
 });
